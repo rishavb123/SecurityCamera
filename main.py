147bfebbc4a2332d6a1803c8c6515088936c3c71
Rishav Bhagat <rishavb123@gmail.com>
2019-04-23 18:20:40 -0400

    shows differences in frame

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+old, frame = cap.read()
+
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    cv2.imshow('frame', gray)
+    cv2.imshow('frame', cv2.subtract(old, frame))
+
+    old = frame
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
